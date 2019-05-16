6d7ff8dc3d37c38533b36ba7355df2fc3574da1c
Eric Rowell <ericrowell@Erics-MacBook-Pro.local>
2019-05-15 18:09:23 -0700

    setup constructor

--- dist/shadow-grid.js
+++ dist/shadow-grid.js
@@ -1,5 +1,5 @@
 /*
- * Shadow Grid v0.0.1
+ * Shadow Grid v0.0.2
  * A high performance table rendering engine for the web
  * Release Date: 05-15-2019
  * https://github.com/ericdrowell/shadow-grid
@@ -25,4 +25,67 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-console.log('here1');
+const ShadowGrid = function(config) {
+  this.rowHeights = config.rowHeights;
+  this.colWidths = config.colWidths;
+  this.rows = config.rows;
+  this.cellRenderer = config.cellRenderer;
+  this.container = config.container;
+  this.viewportWidth = config.viewportWidth;
+  this.viewportHeight = config.viewportHeight;
+
+  this.viewport = document.createElement('div');
+  this.viewport.className = 'shadow-grid-viewport';
+  this.viewport.style.display = 'inline-block';
+  this.viewport.style.width = this.viewportWidth + 'px';
+  this.viewport.style.height = this.viewportHeight + 'px';
+  
+  this.table = document.createElement('div');
+  this.table.style.display = 'inline-block';
+  this.table.className = 'shadow-grid-table';
+
+  this.setTableSize();
+  
+  this.viewport.appendChild(this.table);
+  this.container.appendChild(this.viewport);
+};
+
+ShadowGrid.prototype = {
+  setTableSize: function() {
+    let tableWidth = 0;
+    let tableHeight = 0;
+
+    for (let height of this.rowHeights) {
+      tableHeight += height;
+    }
+    for (let width of this.colWidths) {
+      tableWidth += width;
+    }
+
+    this.tableWidth = tableWidth;
+    this.tableHeight = tableHeight;
+
+    this.table.style.width = tableWidth + 'px';
+    this.table.style.height = tableHeight + 'px';
+  }
+};
+
+// export
+(function (global) {
+  'use strict';
+
+  // AMD support
+  if (typeof define === 'function' && define.amd) {
+    define(function () { return ShadowGrid; });
+  // CommonJS and Node.js module support.
+  } else if (typeof exports !== 'undefined') {
+    // Support Node.js specific `module.exports` (which can be a function)
+    if (typeof module !== 'undefined' && module.exports) {
+      exports = module.exports = ShadowGrid;
+    }
+    // But always support CommonJS module 1.1.1 spec (`exports` cannot be a function)
+    exports.ShadowGrid = ShadowGrid;
+  } else {
+    global.ShadowGrid = ShadowGrid;
+  }
+})(this);
--- src/shadow-grid.js
+++ src/shadow-grid.js
@@ -1,1 +1,64 @@
-console.log('here1');
+const ShadowGrid = function(config) {
+  this.rowHeights = config.rowHeights;
+  this.colWidths = config.colWidths;
+  this.rows = config.rows;
+  this.cellRenderer = config.cellRenderer;
+  this.container = config.container;
+  this.viewportWidth = config.viewportWidth;
+  this.viewportHeight = config.viewportHeight;
+
+  this.viewport = document.createElement('div');
+  this.viewport.className = 'shadow-grid-viewport';
+  this.viewport.style.display = 'inline-block';
+  this.viewport.style.width = this.viewportWidth + 'px';
+  this.viewport.style.height = this.viewportHeight + 'px';
+  
+  this.table = document.createElement('div');
+  this.table.style.display = 'inline-block';
+  this.table.className = 'shadow-grid-table';
+
+  this.setTableSize();
+  
+  this.viewport.appendChild(this.table);
+  this.container.appendChild(this.viewport);
+};
+
+ShadowGrid.prototype = {
+  setTableSize: function() {
+    let tableWidth = 0;
+    let tableHeight = 0;
+
+    for (let height of this.rowHeights) {
+      tableHeight += height;
+    }
+    for (let width of this.colWidths) {
+      tableWidth += width;
+    }
+
+    this.tableWidth = tableWidth;
+    this.tableHeight = tableHeight;
+
+    this.table.style.width = tableWidth + 'px';
+    this.table.style.height = tableHeight + 'px';
+  }
+};
+
+// export
+(function (global) {
+  'use strict';
+
+  // AMD support
+  if (typeof define === 'function' && define.amd) {
+    define(function () { return ShadowGrid; });
+  // CommonJS and Node.js module support.
+  } else if (typeof exports !== 'undefined') {
+    // Support Node.js specific `module.exports` (which can be a function)
+    if (typeof module !== 'undefined' && module.exports) {
+      exports = module.exports = ShadowGrid;
+    }
+    // But always support CommonJS module 1.1.1 spec (`exports` cannot be a function)
+    exports.ShadowGrid = ShadowGrid;
+  } else {
+    global.ShadowGrid = ShadowGrid;
+  }
+})(this);
