0ca777a531a7819302a180eec53cbb15d6568e58
Eric Rowell <ericrowell@Erics-MacBook-Pro.local>
2019-05-15 20:00:42 -0700

    more progress

--- dist/shadow-grid.js
+++ dist/shadow-grid.js
@@ -26,11 +26,13 @@
  * THE SOFTWARE.
  */
 const ShadowGrid = function(config) {
-  this.rowHeights = config.rowHeights;
-  this.colWidths = config.colWidths;
-  this.rows = config.rows;
+  this.cellHeights = config.cellHeights;
+  this.cellWidths = config.cellWidths;
+  this.cells = config.cells;
   this.cellRenderer = config.cellRenderer;
   this.container = config.container;
+  this.numRows = config.numRows;
+  this.numCols = config.numCols;
   this.viewportWidth = config.viewportWidth;
   this.viewportHeight = config.viewportHeight;
 
@@ -39,26 +41,70 @@ const ShadowGrid = function(config) {
   this.viewport.style.display = 'inline-block';
   this.viewport.style.width = this.viewportWidth + 'px';
   this.viewport.style.height = this.viewportHeight + 'px';
+  this.viewport.style.overflow = 'scroll';
   
   this.table = document.createElement('div');
   this.table.style.display = 'inline-block';
   this.table.className = 'shadow-grid-table';
 
   this.setTableSize();
-  
+  this.buildPool();
+  this.updatePool();
+
   this.viewport.appendChild(this.table);
   this.container.appendChild(this.viewport);
 };
 
 ShadowGrid.prototype = {
+  updatePool: function() {
+    //let numRows = this.numRows;
+    //let numCols = this.numCols;
+
+    let cellWidths = this.cellWidths;
+    let cellHeights = this.cellHeights;
+    let cells = this.cells;
+    let cellIndices = [0, 1, 2, 3];
+    let pool = this.pool;
+    
+    for (let cellIndex of cellIndices) {
+      let poolIndex = cellIndex;
+      let cell = cells[cellIndex];
+      let poolCell = pool[poolIndex];
+
+      let rowIndex = 0;
+      let colIndex = 0;
+
+      poolCell.innerHTML = cell.val;
+      poolCell.style.width = cellWidths[colIndex] + 'px';
+      poolCell.style.height = cellHeights[rowIndex] + 'px';
+    }
+  },
+  buildPool: function() {
+    let poolSize = 4;
+
+    this.pool = [];
+
+    let frag = document.createDocumentFragment();
+
+    for (let n =0; n<poolSize; n++) {
+      let cell = document.createElement('div');
+      cell.className = 'shadow-grid-cell';
+      cell.style.display = 'inline-block';
+
+      this.pool.push(cell);
+      frag.appendChild(cell);
+    }
+
+    this.table.appendChild(frag);
+  },
   setTableSize: function() {
     let tableWidth = 0;
     let tableHeight = 0;
 
-    for (let height of this.rowHeights) {
+    for (let height of this.cellHeights) {
       tableHeight += height;
     }
-    for (let width of this.colWidths) {
+    for (let width of this.cellWidths) {
       tableWidth += width;
     }
 
